2eb563de84de989736e766f5d8578557892314f2
Panglot <DobromirBogatev@gmail.com>
2016-08-05 00:44:21 +0300

    Basic Drawing on Canvas with KineticJS

--- Game/JS Files/canvas-draw.js
+++ Game/JS Files/canvas-draw.js
@@ -1,53 +1,103 @@
 (function () {
     var stage = new Kinetic.Stage({
-        container: 'main-drawing-window',
-        width: 920,
-        height: 750
-    }),
-        workplace = document.getElementById('main-drawing-window');
-    var layer = new Kinetic.Layer({
-        clearBeforeDraw: false
-    });
-    var border = new Kinetic.Rect({
-        stroke: "black",
-        strokeWidth: 2,
-        x: 0,
-        y: 0,
-        width: stage.getWidth(),
-        height: stage.getHeight()
-    });
-    var circle = new Kinetic.Circle({
-        radius: 40,
-        fill: 'yellow',
-        stroke: 'black',
-        strokeWidth: 2,
-        x: 100,
-        y: 300
-    });
+            container: 'main-drawing-window',
+            width: 920,
+            height: 750
+        }),
+        workplace = document.getElementById('main-drawing-window'),
+        layer = new Kinetic.Layer({
+            clearBeforeDraw: false
+        }),
+        border = new Kinetic.Rect({
+            stroke: "black",
+            strokeWidth: 2,
+            x: 0,
+            y: 0,
+            width: stage.getWidth(),
+            height: stage.getHeight()
+        }),
+        brush = new Kinetic.Circle({
+            radius: 10,
+            fill: 'yellow',
+            strokeWidth: 2,
+            x: 100,
+            y: 300
+        });
 
+    Input = function () {
+        // this.a = false;
+        // this.b = false;
+        // this.c = false;
+        // this.d = false;
+        // this.e = false;
+        // this.f = false;
+        // this.g = false;
+        // this.h = false;
+        // this.i = false;
+        // this.j = false;
+        // this.k = false;
+        // this.l = false;
+        // this.m = false;
+        // this.n = false;
+        // this.o = false;
+        // this.p = false;
+        // this.q = false;
+        // this.r = false;
+        // this.s = false;
+        // this.t = false;
+        // this.u = false;
+        // this.v = false;
+        // this.w = false;
+        // this.x = false;
+        // this.y = false;
+        // this.z = false;
+        // this.left = false;
+        // this.right = false;
+        // this.up = false;
+        // this.down = false;
+        // this.enter = false;
+        // this.space = false;
+        this.mouseIsDown = false;
+        this.mouseX = 0;
+        this.mouseY = 0;
+        this.offsetX = 0;
+        this.offsetY = 0;
+    };
 
-    workplace.onfocus = document.documentElement.addEventListener('keydown', function (ev) {
-        console.log(ev.which);
-        var duplicate =  circle.clone();
-        if(ev.keyCode === 37 || ev.keyCode === 65){
-            duplicate.move({x: -5, y:0});
+    var input = new Input();
+
+    document.documentElement.onmousedown = function (ev) {
+        input.mouseIsDown = true;
+    };
+
+    document.documentElement.onmouseup = function (ev) {
+        input.mouseIsDown = false;
+    };
+
+    document.documentElement.onmousemove = function (ev) {
+        ev = ev || window.event;
+
+        // input.mouseX = (ev.clientX - workplace.offsetLeft);
+        // input.mouseY = (ev.clientY - workplace.offsetTop);
+        input.mouseX = (ev.offsetX);
+        input.mouseY = (ev.offsetY);
+    };
+
+    var canvasDraw = setInterval(function () {
+        // console.log(input);
+        if (input.mouseIsDown) {
+            workplace.style.cursor = "crosshair";
+            var duplicate = brush.clone();
+            duplicate.setX(input.mouseX);
+            duplicate.setY(input.mouseY);
+            layer.add(duplicate);
+            brush.remove();
+            brush = duplicate;
+            layer.draw();
         }
-        else if(ev.keyCode === 38 || ev.keyCode === 87){
-            duplicate.move({x: 0, y:-5});
-        }
-        else if(ev.keyCode === 39 || ev.keyCode === 68){
-            duplicate.move({x: +5, y:0});
-        }
-        else if(ev.keyCode === 40 || ev.keyCode === 83){
-            duplicate.move({x: 0, y:+5});
-        }
-        layer.add(duplicate);
-        circle.remove();
-        circle = duplicate;
-        layer.draw();
-    });
+    }, 1);
+
 
     layer.add(border);
-    layer.add(circle);
     stage.add(layer);
 })();
