d69b180be1900937ecb022906fcf2764a4a1abcf
Dobromir Bogatev <dobromirbogatev@gmail.com>
2016-08-11 21:28:15 +0300

    Added color remember for eraser.

--- Game/JS Files/tools-bar.js
+++ Game/JS Files/tools-bar.js
@@ -21,7 +21,7 @@ function toolbar(brush) {
 
 
     var redrawToolbar = function () {
-        ctx.clearRect(0,0,canvas.width,canvas.height);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(pen, 10, canvas.height - pen.height);
         ctx.drawImage(marker, 80, canvas.height - marker.height);
         ctx.drawImage(crayon, 150, canvas.height - crayon.height);
@@ -53,62 +53,96 @@ function toolbar(brush) {
         var mouseY = (ev.clientY - canvas.offsetTop + document.body.scrollTop);
 
 
-        if(mouseX > 10 && mouseX < pen.width+10 && mouseY > canvas.height - pen.height && mouseY < canvas.height){
+        if (mouseX > 10 && mouseX < pen.width + 10 && mouseY > canvas.height - pen.height && mouseY < canvas.height) {
             brush.type = 'pen';
+            if(brush.color === 'white'){brush.color = brush.prevColor;}
         }
-        else if(mouseX > 80 && mouseX < marker.width+80 && mouseY > canvas.height - marker.height && mouseY < canvas.height){
+        else if (mouseX > 80 && mouseX < marker.width + 80 && mouseY > canvas.height - marker.height && mouseY < canvas.height) {
             brush.type = 'marker';
+            if(brush.color === 'white'){brush.color = brush.prevColor;}
         }
-        else if(mouseX > 150 && mouseX < crayon.width+150 && mouseY > canvas.height - crayon.height && mouseY < canvas.height){
+        else if (mouseX > 150 && mouseX < crayon.width + 150 && mouseY > canvas.height - crayon.height && mouseY < canvas.height) {
             brush.type = 'crayon';
+            if(brush.color === 'white'){brush.color = brush.prevColor;}
         }
-        else if(mouseX > 220 && mouseX < eraser.width+220 && mouseY > canvas.height - eraser.height && mouseY < canvas.height){
+        else if (mouseX > 220 && mouseX < eraser.width + 220 && mouseY > canvas.height - eraser.height && mouseY < canvas.height) {
             brush.type = 'eraser';
             brush.color = 'white';
         }
-        else if(mouseX > 329 && mouseX < 345 && mouseY > 13 && mouseY < 28){
+        else if (mouseX > 329 && mouseX < 345 && mouseY > 13 && mouseY < 28) {
             brush.thickness = 8;
         }
-        else if(mouseX > 325 && mouseX < 347 && mouseY > 40 && mouseY < 59){
+        else if (mouseX > 325 && mouseX < 347 && mouseY > 40 && mouseY < 59) {
             brush.thickness = 16;
         }
-        else if(mouseX > 323 && mouseX < 350 && mouseY > 74 && mouseY < 98){
+        else if (mouseX > 323 && mouseX < 350 && mouseY > 74 && mouseY < 98) {
             brush.thickness = 28;
         }
-        else if(mouseX > 320 && mouseX < 356 && mouseY > 110 && mouseY < 144){
+        else if (mouseX > 320 && mouseX < 356 && mouseY > 110 && mouseY < 144) {
             brush.thickness = 40;
         }
-        else if(mouseX > 415 && mouseX < 440 && mouseY > 80 && mouseY < 105){
-            brush.color = '#FFFF66';
+        else if (mouseX > 415 && mouseX < 440 && mouseY > 80 && mouseY < 105) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#FFFF66';
+                brush.prevColor = '#FFFF66';
+            }
         }
-        else if(mouseX > 457 && mouseX < 482 && mouseY > 80 && mouseY < 105){
-            brush.color = '#FF9900';
+        else if (mouseX > 457 && mouseX < 482 && mouseY > 80 && mouseY < 105) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#FF9900';
+                brush.prevColor = '#FF9900';
+            }
         }
-        else if(mouseX > 499 && mouseX < 522 && mouseY > 80 && mouseY < 105){
-            brush.color = '#FF3300';
+        else if (mouseX > 499 && mouseX < 522 && mouseY > 80 && mouseY < 105) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#FF3300';
+                brush.prevColor = '#FF3300';
+            }
         }
-        else if(mouseX > 539 && mouseX < 563 && mouseY > 80 && mouseY < 105){
-            brush.color = '#ACACAC';
+        else if (mouseX > 539 && mouseX < 563 && mouseY > 80 && mouseY < 105) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#ACACAC';
+                brush.prevColor = '#ACACAC';
+            }
         }
-        else if(mouseX > 581 && mouseX < 605 && mouseY > 80 && mouseY < 105){
-            brush.color = '#765000';
+        else if (mouseX > 581 && mouseX < 605 && mouseY > 80 && mouseY < 105) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#765000';
+                brush.prevColor = '#765000';
+            }
         }
-        else if(mouseX > 415 && mouseX < 440 && mouseY > 121 && mouseY < 146){
-            brush.color = '#339933';
+        else if (mouseX > 415 && mouseX < 440 && mouseY > 121 && mouseY < 146) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#339933';
+                brush.prevColor = '#339933';
+            }
         }
-        else if(mouseX > 457 && mouseX < 482 && mouseY > 121 && mouseY < 146){
-            brush.color = '#66FF99';
+        else if (mouseX > 457 && mouseX < 482 && mouseY > 121 && mouseY < 146) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#66FF99';
+                brush.prevColor = '#66FF99';
+            }
         }
-        else if(mouseX > 499 && mouseX < 522 && mouseY > 121 && mouseY < 146){
-            brush.color = '#33CCFF';
+        else if (mouseX > 499 && mouseX < 522 && mouseY > 121 && mouseY < 146) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#33CCFF';
+                brush.prevColor = '#33CCFF';
+            }
         }
-        else if(mouseX > 539 && mouseX < 563 && mouseY > 121 && mouseY < 146){
-            brush.color = '#003399';
+        else if (mouseX > 539 && mouseX < 563 && mouseY > 121 && mouseY < 146) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#003399';
+                brush.prevColor = '#003399';
+            }
         }
-        else if(mouseX > 581 && mouseX < 605 && mouseY > 121 && mouseY < 146){
-            brush.color = '#000000';
+        else if (mouseX > 581 && mouseX < 605 && mouseY > 121 && mouseY < 146) {
+            if (brush.type !== 'eraser') {
+                brush.color = '#000000';
+                brush.prevColor = '#000000';
+            }
+            
         }
-        else if(mouseX > 642 && mouseX < 704 && mouseY > 69 && mouseY < 148){
+        else if (mouseX > 642 && mouseX < 704 && mouseY > 69 && mouseY < 148) {
             brush.clear = true;
         }
         redrawToolbar();
